7b943f6f626c7b794b5ff8cc498dc49018ecd2cc
Mohammed Zulfiqar <150642443+zulfiqar-uxd@users.noreply.github.com>
2024-07-23 08:07:10 +0000

    Pending changes exported from your codespace

--- src/components/layout/vertical/FooterContent.jsx
+++ src/components/layout/vertical/FooterContent.jsx
@@ -1,22 +1,30 @@
 'use client'
 
-// Next Imports
-import Link from 'next/link'
-
 // Third-party Imports
 import classnames from 'classnames'
+
+// Hook Imports
+import useVerticalNav from '@menu/hooks/useVerticalNav'
 
 // Util Imports
 import { verticalLayoutClasses } from '@layouts/utils/layoutClasses'
 
 const FooterContent = () => {
+  // Hooks
+  const { isBreakpointReached } = useVerticalNav()
+
   return (
     <div
-      className={classnames(verticalLayoutClasses.footerContent, 'flex items-center justify-between flex-wrap gap-4')}
+      className={classnames(
+        verticalLayoutClasses.footerContent,
+        'flex items-center justify-between flex-wrap gap-4 text-xs'
+      )}
     >
-      <p>
-        <span>{`© ${new Date().getFullYear()} Oncure Bio Solutions. All rights reserved.`}</span>
-      </p>
+      {!isBreakpointReached && (
+        <p>
+          <span>{`© ${new Date().getFullYear()} Oncure Bio Solutions. All rights reserved.`}</span>
+        </p>
+      )}
     </div>
   )
 }
